620611a5c4f11d4b2fd117a0b66a79f617167743
adamggrover <93373074+adamggrover@users.noreply.github.com>
2024-02-11 15:03:03 +0000

    add initial files

---
+++ footer.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The footer
+ *
+ * @package Bathe
+ */
+
+?>
+</main>
+
+	<?php get_sidebar(); ?>
+</div>
+
+<footer class="footer">
+	<p class="copyright">© 2020 the contributors of Bathe under the GPLv2 license.</p>
+</footer>
+
+</div> <!-- close #outer-container -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,154 @@
+<?php
+/**
+ * Bathe functions
+ *
+ * @package Bathe
+ */
+
+/**
+ * Set up theme defaults and registers support for various WordPress feaures.
+ */
+add_action(
+	'after_setup_theme',
+	function () {
+		load_theme_textdomain( 'bathe', get_theme_file_uri( 'languages' ) );
+
+		add_theme_support( 'automatic-feed-links' );
+		add_theme_support( 'title-tag' );
+		add_theme_support( 'post-thumbnails' );
+		add_theme_support(
+			'html5',
+			array(
+				'search-form',
+				'comment-form',
+				'comment-list',
+				'gallery',
+				'caption',
+			)
+		);
+		add_theme_support(
+			'post-formats',
+			array(
+				'aside',
+				'image',
+				'video',
+				'quote',
+				'link',
+			)
+		);
+		add_theme_support(
+			'custom-background',
+			apply_filters(
+				'bathe_custom_background_args',
+				array(
+					'default-color' => 'ffffff',
+					'default-image' => '',
+				)
+			)
+		);
+
+		// Add theme support for selective refresh for widgets.
+		add_theme_support( 'customize-selective-refresh-widgets' );
+
+		/**
+		 * Add support for core custom logo.
+		 *
+		 * @link https://codex.wordpress.org/Theme_Logo
+		 */
+		add_theme_support(
+			'custom-logo',
+			array(
+				'height'      => 200,
+				'width'       => 250,
+				'flex-width'  => true,
+				'flex-height' => true,
+			)
+		);
+
+		register_nav_menus(
+			array(
+				'primary' => __( 'Primary Menu', 'bathe' ),
+			)
+		);
+	}
+);
+
+/**
+ * Set the content width in pixels, based on the theme's design and stylesheet.
+ *
+ * Priority 0 to make it available to lower priority callbacks.
+ *
+ * @global int $content_width
+ */
+add_action(
+	'after_setup_theme',
+	function () {
+		$GLOBALS['content_width'] = apply_filters( 'bathe_content_width', 960 );
+	},
+	0
+);
+
+/**
+ * Register widget area.
+ */
+add_action(
+	'widgets_init',
+	function () {
+		register_sidebar(
+			array(
+				'name'          => __( 'Sidebar', 'bathe' ),
+				'id'            => 'sidebar-1',
+				'description'   => '',
+				'before_widget' => '<section id="%1$s" class="widget %2$s">',
+				'after_widget'  => '</section>',
+				'before_title'  => '<h2 class="widget-title">',
+				'after_title'   => '</h2>',
+			)
+		);
+	}
+);
+
+/**
+ * Enqueue scripts and styles.
+ */
+add_action(
+	'wp_enqueue_scripts',
+	function () {
+		wp_enqueue_style( 'bathe', get_theme_file_uri( 'assets/css/main.css' ), array(), '3.0.1' );
+		wp_enqueue_style( 'tailwind', get_theme_file_uri( 'assets/css/tailwind.css' ), array(), '3.3.2' );
+
+		wp_enqueue_script( 'bathe', get_theme_file_uri( 'assets/js/main.js' ), array(), '3.0.1', true );
+
+		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+			wp_enqueue_script( 'comment-reply' );
+		}
+	}
+);
+
+/**
+ * Load WooCommerce compatibility file.
+ */
+if ( class_exists( 'WooCommerce' ) ) {
+	function mytheme_add_woocommerce_support() {
+		add_theme_support( 'woocommerce',
+		array(
+			'thumbnail_image_width' => 100,
+			'thumbnail_image_height' => 250,
+			'single_image_width'    => 300,
+			'product_grid'          => array(
+				'default_rows'    => 3,
+				'min_rows'        => 1,
+				'default_columns' => 4,
+				'min_columns'     => 1,
+				'max_columns'     => 4,
+			),
+		) );
+
+
+
+		add_theme_support( 'wc-product-gallery-zoom' );
+		add_theme_support( 'wc-product-gallery-lightbox' );
+		add_theme_support( 'wc-product-gallery-slider' );
+	}
+	add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
+}
